52cb501b1665065e62c0d8172d70ea9ae5f5c978
Alex Beacon <a.beacon42@gmail.com>
2025-04-06 12:33:09 -0400

    minor tweaks

--- data/etl/run_etl_azure_docs_raw.sh
+++ data/etl/run_etl_azure_docs_raw.sh
@@ -6,35 +6,32 @@
 # todo: incorporate this script into main pipeline
 # todo: add more repos - architecture-center, azure-docs-sdk-python, azure-docs-cli, +580 more
 # todo: add logic -->
-'''
-    if !repo exists, 
-        shallow clone it; 
-        echo "repo does not exist, shallow cloning it. this might take a while." || error_exit "Repo does not exist & failed to shallow clone the repository."
-        exit 0;
-    else, if there are no changes, 
-        echo "no changes to pull, exiting." || error_exit "No changes to pull."
-        exit 0; 
-    else, pull changes to main
-        echo "pulling changes to main." || error_exit "Failed to pull changes to main."
-        exit 0;
-    fi
-'''
+#    if !repo exists, 
+#        shallow clone it; 
+#        echo "repo does not exist, shallow cloning it. this might take a while." || error_exit "Repo does not exist & failed to shallow clone the repository."
+#        exit 0;
+#    else, if there are no changes, 
+#        echo "no changes to pull, exiting." || error_exit "No changes to pull."
+#        exit 0; 
+#    else, pull changes to main
+#        echo "pulling changes to main." || error_exit "Failed to pull changes to main."
+#        exit 0;
+#    fi
 
-# Function to handle errors
 error_exit() {
     echo "Error: $1" >&2
     exit 1
 }
 
-TARGET_DIR="Users/alex/Desktop/code_projects/commuter_copilot/local_files/data/raw/azure-docs"
 
-if [ -d "$TARGET_DIR" ]; then
-    echo "fetching latest changes from upstream's main branch"
-    cd "$TARGET_DIR" || error_exit "Failed to change directory to $TARGET_DIR."
+if [ -d "$AZ_DOCS_TARGET_DIR" ]; then
+    echo "Fetching latest changes from upstream's main branch"
+    cd "$AZ_DOCS_TARGET_DIR" || error_exit "Failed to change directory to $AZ_DOCS_TARGET_DIR."
 fi
 
+git fetch --depth 1 origin main || error_exit "Failed to fetch shallow copy of main branch."
 git checkout main || error_exit "Failed to checkout main branch."
-git pull || error_exit "Failed to pull upstream changes."
-echo "Latest changes merged successfully merged to local main."
+git reset --hard origin/main || error_exit "Failed to reset local main branch."
+echo "Latest shallow changes merged successfully from origin/main."
 
 exit 0
